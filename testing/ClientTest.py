18d48dce59b4ef4e355efc565d5f99c7d74fea34
AGBR121 <angel.burbano@utp.edu.co>
2025-11-03 20:30:21 -0500

    ajustes para realizar debug

--- testing/ClientTest.py
+++ testing/ClientTest.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from datetime import date
 from modelo.Cliente import Cliente
 from modelo.Factura import Factura
